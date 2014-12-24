98e53cf799d2d50fde10d3e28699bec6f363f98e
Xtreeem - Hugo <Hugo.tinglof@gmail.com>
2014-12-24 14:18:56 +0100

    added missing classes

---
+++ Project PewPew/Project PewPew/Project PewPew/Core/Actor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_PewPew
+{
+    abstract class Actor : GameObject
+    {
+        public Vector2 Velocity { get; protected set; }
+        protected float Rotation { get; set; }
+        protected float MovementSpeed { get; set; }
+        protected Color Color { get; set; }
+        protected Vector2 Origin
+        {
+            get
+            {
+                return (new Vector2(Texture.Width / 2, Texture.Height / 2));
+            }
+        }
+        public override void Update(GameTime GameTime)
+        {
+            Move(GameTime);
+            base.Update(GameTime);
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch SpriteBatch)
+        {
+            SpriteBatch.Draw(Texture, Position, null, Color, Rotation, Origin, 1f, Microsoft.Xna.Framework.Graphics.SpriteEffects.None, 0f);
+        }
+
+
+        private void Move(GameTime GameTime)
+        {
+            if (Velocity != Vector2.Zero)
+            {
+                Rotation = (float)Math.Atan2(Velocity.Y, Velocity.X);
+            }
+            Position += (Velocity * (GameTime.ElapsedGameTime.Milliseconds * MovementSpeed));
+        }
+    }
+}
---
+++ Project PewPew/Project PewPew/Project PewPew/Player.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_PewPew
+{
+    class Player : Actor
+    {
+        public int PlayerIndex { get; private set; }
+        public Player(Vector2 StartPos, int PlayerIndex)
+        {
+            Texture = TextureManager.Player;
+            this.PlayerIndex = PlayerIndex;
+            Position = StartPos;
+            MovementSpeed = 0.6f;
+            Color = Color.BlueViolet;
+        }
+
+        public override void Update(GameTime GameTime)
+        {
+            Velocity = InputManager.Get_Movement_Direction(PlayerIndex);
+            base.Update(GameTime);
+        }
+
+
+    }
+}

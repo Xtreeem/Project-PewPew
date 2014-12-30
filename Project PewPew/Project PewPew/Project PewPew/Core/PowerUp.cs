73377638ed2abb79e1a677c1f6f4edbc1b2b95f6
Xtreeem <hugo.tinglof@gmail.com>
2014-12-30 01:17:58 +0100

    Implemented base of the powerup system

---
+++ Project PewPew/Project PewPew/Project PewPew/Core/PowerUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_PewPew
+{
+    class PowerUp : Actor
+    {
+
+        public void PickedUp(ref Player Player)
+        {
+            //Player.Weapon.AttackRate += 0.4; etc
+            Die();
+        }
+
+
+    }
+}

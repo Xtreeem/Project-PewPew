14627546dc1456e7ed69f9f21b61196863704c45
Xtreeem - Hugo <Hugo.tinglof@gmail.com>
2015-01-05 23:25:35 +0100

    Implemented QuadTree class from CodeProject.com

---
+++ Project PewPew/Project PewPew/Project PewPew/Core/QuadTree/IHasRect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Project_PewPew
+{
+    public interface IHasRect
+    {
+        RectangleF Rectangle { get; }
+    }
+}
